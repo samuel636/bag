666d114106103f97a49a785cdba646207d8e8deb
samuel636 <samuel22008@student.hamk.fi>
2023-02-14 21:09:46 +0200

    trial
    trie

--- home.php
+++ home.php
@@ -5,5 +5,7 @@
     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, doloremque rem vel ex cumque temporibus deleniti officiis assumend
     a omnis, perspiciatis molestiae saepe consequatur autem rerum quos impedit animi accusamudfsfsds corrupti.
 </p>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A magni ex laborum perspiciatis nesciunt eum delectus e
+    xcepturi neque libero! Maxime soluta illo veniam natus fugit doloremque amet quidem, laudantium deleniti?</p>
 <p>thjdfksdf djfkdsfhjsdf sjdkfhsjdf</p>
 <?php include "footer.php" ?>
