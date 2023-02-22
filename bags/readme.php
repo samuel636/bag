2bfd118165313f1bb39f68ef646f5f2cc742b3c7
Eusabious12 <eusabious22000@student.hamk.fi>
2023-02-22 19:22:54 +0200

    Create readme.php
    read

---
+++ bags/readme.php
@@ -0,0 +1,35 @@
+?php
+$title="Reading Data from the data base";
+include '..header.php';
+include 'db.php';
+$sql = "select * from userinfo ";
+$result =$conn->query($sq1);
+if($result->num_rows > 0){
+    
+    echo "<table class = 'table'>
+    <tr><th>name</tr></tr><tr><th>email</tr></tr><tr><th>address</tr></tr><tr><th>userid</tr></tr>";
+    
+    while($row=$result->fetech_assoc()){
+      
+        echo "
+ <tr>
+ <td><a href ='updatesingle.php?=$row[id]'>$row[id]</a></td>
+ <td>$row[name]</td>
+ <td>$row[email]</td>
+ <td>$row[address]</td>
+ <td>$row[userid]</td>
+    </tr>
+    ";
+}
+   echo "</table>";
+
+}
+else
+{
+echo "<No Result>";
+
+}
+$conn->close();
+include '../footer.php';
+
+?>
