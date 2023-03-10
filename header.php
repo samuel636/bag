c63f20c2ec46fdb14d74dbda76292818bf8db08c
samuel636 <samuel22008@student.hamk.fi>
2023-03-10 13:15:18 +0200

    edit

---
+++ header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel= stylesheet  href ="vm.css">
+  </head>
+  <body>
+        <div class="container">
+     <div class="row">
+         <div class="col-6">
+         
+        <h1>The Hand Bag company</h1>  
+       <img class="img-fluid float " src="log/c.jpg" alt="logooo" > 
+      </div>     
+   </div> 
+       <div class ="row">
+       <div class="box">
+        <ul type="none">
+        <li><a href="home.php"> home</a></li>
+        <li><a href="one.php"> order</a></li>
+        <li><a href="meenbag.html"> Men</a></li>
+       
+      
+</ul>
+</div>         
+</div>
+</div>
